d29decb50f1f9ff4943266050358949a42eb5498
AidilDaniel <aidilvendetta@gmail.com>
2022-04-03 23:26:28 +0800

    Update server.php

--- Login Page/server.php
+++ Login Page/server.php
@@ -6,10 +6,10 @@
     $errors = array();
     
     // connect to the database
-    $db = mysqli_connect('localhost', 'root', '', 'register');
+    $db = mysqli_connect('localhost', 'root', '', 'user_data_info');
 
     // if the register button is clicked
-    if (isset($_POST['register'])) {
+    if (isset($_POST['user_data_info'])) {
         $username = mysqli_real_escape_string($db, $_POST['username']);
         $useremail = mysqli_real_escape_string($db, $_POST['useremail']);
         $userpassword = mysqli_real_escape_string($db, $_POST['userpassword']);
@@ -17,7 +17,7 @@
         //if there are no errors, save user to database
         if (count($errors) == 0) {
             $userpassword = md5($userpassword); //encrypt password before storing in database (security)
-            $sql = "INSERT INTO user (username, useremail, userpassword) VALUES ('$username', '$useremail', '$userpassword')";
+            $sql = "INSERT INTO registration (username, useremail, userpassword) VALUES ('$username', '$useremail', '$userpassword')";
             mysqli_query($db, $sql);
 
             $_SESSION['username'] = $username;
