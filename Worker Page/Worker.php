e3d74826ce8c6c142d77491a77151b56d2aeaf81
susshi <anwarazhad00@gmail.com>
2022-04-10 01:38:07 +0800

    create file for worker page and create worker page

---
+++ Worker Page/Worker.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width", initial-scale=1.0>
+        <title>Worker Page</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="worker.css">
+    </head>
+
+    <body>
+        <div class=navigation>
+            <ul>
+                <div class=logo>
+                    <label class="logo"><img src="images/logo.png"></a></label>
+                </div>
+                
+                <li>
+                    <a class="username" href="#">
+                        <span class="icon"><i class="fa fa-user-circle" aria-hidden="true"></i></span>
+                        <span class="title">User Name</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a class="home" href="#">
+                        <span class="icon"><i class="fa fa-home" aria-hidden="true"></i></span>
+                        <span class="title">Home</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a class="notification" href="#">
+                        <span class="icon"><i class="fa fa-bell" aria-hidden="true"></i></span>
+                        <span class="title">Notification</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a class="inbox" href="#">
+                        <span class="icon"><i class="fa fa-inbox" aria-hidden="true"></i></span>
+                        <span class="title">Inbox</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a class="setting" href="#">
+                        <span class="icon"><i class="fa fa-cog" aria-hidden="true"></i></span>
+                        <span class="title">Setting</span>
+                    </a>
+                </li>
+                
+            </ul>
+        </div>
+    </body>
+</html>
