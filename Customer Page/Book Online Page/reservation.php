77cd67d1083d6cbb121e5a389964bf557bcb632a
susshi <anwarazhad00@gmail.com>
2022-04-13 00:31:54 +0800

    create booking site for booking page

---
+++ Customer Page/Book Online Page/reservation.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Imanstudio.com/Book</title>
+    
+    <link rel="stylesheet" href="book.css">
+
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+  </head>
+  <body>
+    <div class="full-page">
+      <div class="sub-page">
+
+      <div class="navigation-bar">
+          <nav>
+            <input type="checkbox" id="check">
+            <label for="check" class="checkbtn">
+              <i class="fas fa-bars"></i>
+            </label>
+            <label class="logo"><a href="#"><img src="images/logo.png"></a></label>
+
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Album</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#" style="color: #ffde00;">Book Online</a></li>
+              <li><a href="#">Login</a></li>
+            </ul>
+          </nav>
+        </div>
+
+        <div class=navigation>
+            <ul>
+                
+              <li>
+                <a class="profile" href="#">
+                    <span class="icon"><i class="fa fa-user-circle" aria-hidden="true"></i></span>
+                    <span class="title">Profile</span>
+                </a>
+              </li>
+
+              <li>
+                <a class="reservation" href="#" style="color: #ffde00;">
+                    <span class="icon"><i class="fa fa-file" aria-hidden="true"></i></span>
+                    <span class="title">Reservation</span>
+                </a>
+              </li>
+
+              <li>
+                  <a class="booking" href="#">
+                    <span class="icon"><i class="fa fa-calendar" aria-hidden="true"></i></i></span>
+                    <span class="title">Booking</span>
+                  </a>
+              </li>
+
+            </ul>
+        </div>
+
+        <div class="main">
+            <h1></h1>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
